89909ca8bfd55eff11dc0ae824677f71bb73ad0d
xjullhcx <logicoreautomation@gmail.com>
2022-03-03 20:58:29 -0500

    subiendo el estudio de js

---
+++ HTML/JS/estudio.js
@@ -0,0 +1,21 @@
+var MiPrimerVAriable = 5;
+console.log(MiPrimerVAriable);
+if(MiPrimerVAriable===5)
+{
+    console.log("ingreso al if");
+}
+else 
+{ 
+    console.log("ingreso al else");
+
+}
+
+
+var MiSegundaVariable = new String('hola tu')
+console.log(MiSegundaVariable);
+console.log(typeof MiPrimerVAriable,typeof MiSegundaVariable);
+var suma = '5+5';
+console.log(eval(suma));
+
+var ObjetoAndrea = { nombre:'andrea',edad:32}
+console.log(ObjetoAndrea)
